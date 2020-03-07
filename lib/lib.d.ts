13366f826ce8c281d35ab3cb92f1931f2f385a32
James Ives <iam@jamesiv.es>
2020-03-07 10:40:54 -0500

    Build

--- lib/lib.d.ts
+++ lib/lib.d.ts
@@ -2,4 +2,4 @@ import {ActionInterface} from './constants'
 import {generateExport, retrieveData} from './fetch'
 /** Initializes and runs the action. */
 export default function run(configuration: ActionInterface): Promise<void>
-export {retrieveData, generateExport}
+export {retrieveData, generateExport, ActionInterface}
