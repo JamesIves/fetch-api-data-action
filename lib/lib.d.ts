9f4d348298d1d689d7015208aba0a7c4d63a5eb8
James Ives <iam@jamesiv.es>
2020-04-01 11:51:07 -0400

    Correct

--- lib/lib.d.ts
+++
@@ -1,8 +0,0 @@
-import { ActionInterface } from './constants';
-import { generateExport, retrieveData } from './fetch';
-/** Initializes and runs the action.
- *
- * @param {ActionInterface} configuration - The configuration object.
- */
-export default function run(configuration: ActionInterface): Promise<void>;
-export { retrieveData, generateExport, ActionInterface };
--- lib/util.d.ts
+++
@@ -1,3 +0,0 @@
-import { ActionInterface } from './constants';
-export declare const isNullOrUndefined: (value: string | null | undefined) => boolean;
-export declare const hasRequiredParameters: (action: ActionInterface) => void;
