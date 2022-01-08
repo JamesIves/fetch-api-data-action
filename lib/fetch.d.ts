07fdf63aad703040a537900787cc76840dfc5738
James Ives <iam@jamesiv.es>
2022-01-08 20:33:12 +0000

    Deploy Production Code for Commit 953ec50fc1a4550c5a43ea0af9181e824de10751 🚀

--- lib/fetch.d.ts
+++ lib/fetch.d.ts
@@ -1,4 +1,4 @@
 import 'cross-fetch/polyfill';
 import { DataInterface, ExportInterface, Status } from './constants';
-export declare function retrieveData({ debug: requestDebug, endpoint, configuration, auth, isTokenRequest, retry }: DataInterface): Promise<Record<string, unknown>>;
+export declare function retrieveData({ debug: requestDebug, endpoint, configuration, auth, isTokenRequest, retry }: DataInterface): Promise<string>;
 export declare function generateExport({ data, saveLocation, saveName }: ExportInterface): Promise<Status>;
