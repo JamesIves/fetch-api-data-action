4835d18081e83f0e6586614bb75dffc62176db65
James Ives <iam@jamesiv.es>
2022-01-10 09:25:27 -0500

    Update fetch.ts

--- src/fetch.ts
+++ src/fetch.ts
@@ -65,11 +65,18 @@ export async function retrieveData({
 /* Saves the data to the local file system and exports an environment variable containing the retrieved data. */
 export async function generateExport({
   data,
+  format,
   saveLocation,
   saveName
 }: ExportInterface): Promise<Status> {
   info('Saving the data... 📁')
+  const file = `${saveLocation ? saveLocation : 'fetch-api-data-action'}/${
+    saveName ? saveName : 'data'
+  }.${format ? format : 'json'}`
   await mkdirP(`${saveLocation ? saveLocation : 'fetch-api-data-action'}`)
+  await fs.writeFile(file, data, 'utf8')
+
+  info(`Saved ${file} 💾`)
   await fs.writeFile(
     `${saveLocation ? saveLocation : 'fetch-api-data-action'}/${
       saveName ? saveName : 'data'
@@ -77,6 +84,7 @@ export async function generateExport({
     data,
     'utf8'
   )
+
   exportVariable('fetch-api-data', data)
 
   return Status.SUCCESS
